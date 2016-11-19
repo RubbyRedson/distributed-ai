567732b02c9918d2950492c7c490cc2f59cbddd9
RubbyRedson <gurnik2009@gmail.com>
2016-11-19 19:57:41 +0100

    Strategies fixes

--- First/src/main/java/stategies/BuyLowQuality.java
+++ First/src/main/java/stategies/BuyLowQuality.java
@@ -5,19 +5,21 @@ package stategies;
  */
 public class BuyLowQuality implements BidderStrategy {
     int ourTrueValue;
+    private static final int COEFF = 10;
+    private static final int LOWEST_PRICE_FOR_HQ = 200;
 
     @Override
     public boolean propose(int price, int budget) {
         if (price > budget) return false;
         boolean lowQuality = determineQuality(price);
-        ourTrueValue = budget / 10;
+        ourTrueValue = budget;
         System.out.println("BuyLow: Our true value is " + ourTrueValue + ", price is " + price +
                 ". Low quality is " + lowQuality);
-        return lowQuality && price <= ourTrueValue;
+        return lowQuality && price < ourTrueValue;
     }
 
     private boolean determineQuality(int price) {
-        if (price > 200) return false; //probably high quality
+        if (price > LOWEST_PRICE_FOR_HQ) return false; //probably high quality
         return true; //buy when price is less than 200
     }
 }
